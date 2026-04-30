a0c17eabc7bab481f97a510c5e6685061d851654
omwi <bsuiromwi@gmail.com>
2026-04-30 13:10:41 +0300

    fix: improve shift badge text contrast

--- src/features/employees/components/shift-badge.tsx
+++ src/features/employees/components/shift-badge.tsx
@@ -5,13 +5,10 @@ import type { Shift } from "../types/types"
 export default function ShiftBadge({ shift }: { shift: Shift }) {
   return (
     <span
-      className={cn(
-        "flex size-8 items-center justify-center rounded-full text-white",
-        {
-          "bg-green-400": shift === "A",
-          "bg-sky-400": shift === "B",
-        }
-      )}
+      className={cn("flex size-8 items-center justify-center rounded-full", {
+        "bg-green-300": shift === "A",
+        "bg-sky-300": shift === "B",
+      })}
     >
       {shift}
     </span>
